372f881f99732899814893f5660fbb4a3e98b34d
adhermit <maharjanraman456@gmail.com>
2024-12-02 20:14:06 +0100

    update

---
+++ Workshop4/index.php
@@ -0,0 +1,40 @@
+<?php
+$mainMenu = [
+    ["name" => 'Home', "link" => 'index.php'],
+    ["name" => 'Contact', "link" => 'contact.php']
+];
+
+$footerMenu = [
+    ["name" => 'Terms and Condition', "link" => 'terms.php'],
+    ["name" => 'Legal Policies', "link" => 'legal.php']
+];
+
+function generateMenu($menu)
+{
+    foreach ($menu as $mainMenu) { ?>
+        <ul>
+            <li><a href="<?= $mainMenu['link']; ?>"><?= $mainMenu['name']; ?></li>
+        </ul>
+<?php }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    generateMenu($mainMenu);
+    ?>
+</body>
+    <?php
+    generateMenu($footerMenu);
+    ?>
+
+</html>
---
+++ Workshop4/legal.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Legal Policies</h1>
+    <p>You must follow our legal policies. In case of volience of our leagal policies you may have to give heavy compansiation or can even get restigated from our company, So please be careful. Thank you.</p>
+</body>
+
+</html>
