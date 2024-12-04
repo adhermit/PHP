ec9ff66aa42c5401357d083d40981bf1e4b00513
adhermit <maharjanraman456@gmail.com>
2024-12-04 14:01:17 +0100

    new

--- workshop5/libs/games.php
+++ workshop5/libs/games.php
@@ -6,7 +6,7 @@ function getGames(): array
     $games = [
         ["name" => 'GTA', "editor" => "Rockstar Games", "description" => "GTA is an open-world action-adventure game series known for its immersive storylines, vast urban landscapes, and gameplay."],
         ["name" => 'Class of Clans', "editor" => "Supercell", "description" => "Clash of Clans is a strategy-based mobile game where players build and expand their villages, train armies, and engage in battles with other players or clans."],
-        ["name" => 'Call break', "editor" => "Activision", "description" => "Call of Duty is a first-person shooter series that spans various war scenarios, from historical World War II settings to futuristic battles."],
+        ["name" => 'Call of duty', "editor" => "Activision", "description" => "Call of Duty is a first-person shooter series that spans various war scenarios, from historical World War II settings to futuristic battles."],
     ];
 
     return $games;
