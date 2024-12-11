b03ede086935146300a3f673e92938cb80f56e6f
adhermit <maharjanraman456@gmail.com>
2024-12-11 13:59:32 +0100

    followup

--- Exercise11_part3(session) copy/profile.php
+++ Exercise11_part3(session) copy/profile.php
@@ -1,4 +1,10 @@
-<?php include_once "header.php" ?>
+<?php 
+if (!isset($_SESSION["email"])){
+    header("Location: login.php");
+}
+
+include_once "header.php" 
+?>
 <h1>My Profile</h1>
 <h2><?= $_SESSION['email'] ?></h2>
 <?php include_once "footer.php" ?>
