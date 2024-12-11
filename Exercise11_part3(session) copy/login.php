3331ca1e627ead8d49a03e24665e0a41168b4ea0
adhermit <maharjanraman456@gmail.com>
2024-12-11 14:36:47 +0100

    new

--- Exercise11_part3(session) copy/login.php
+++ Exercise11_part3(session) copy/login.php
@@ -4,20 +4,20 @@
 if (isset($_SESSION['email'])) {
     include_once "profile.php";
     echo '<p><a href="logout.php">Logout</a></p>';
-} else {?>
+} else { ?>
 
-<form action="" method="post">
-    <P>
-        <label for="email">Email: </label>
-        <input type="email" name="email" id="email"><br>
-    </P>
-    <P>
-        <label for="password">Password: </label>
-        <input type="password" name="password" id="password"><br>
-    </P>
-    <button type="submit">login</button>
+    <form action="" method="post">
+        <P>
+            <label for="email">Email: </label>
+            <input type="email" name="email" id="email"><br>
+        </P>
+        <P>
+            <label for="password">Password: </label>
+            <input type="password" name="password" id="password"><br>
+        </P>
+        <button type="submit">login</button>
 
-</form>
+    </form>
 <?php } ?>
 
 <?php
@@ -27,13 +27,14 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     $userEmail = $_POST['email'];
     $userPassword = $_POST['password'];
 
+    // var_dump($userEmail, $userPassword);
+
     if ($userEmail === $email && $userPassword === $password) {
         session_regenerate_id(true);
         $_SESSION['email'] = $userEmail;
         header("Location: profile.php");
     } else {
-        echo " You are not allowed";
-        header("Location: Login");
+        echo "Please enter a valid email and password";
     }
 } ?>
 
