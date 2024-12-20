7a52c6e9571510c01926e618ee2db55c4b03b217
adhermit <maharjanraman456@gmail.com>
2024-12-20 14:00:45 +0100

    sign up

--- Exercise13_Database_11partC/signup.php
+++ Exercise13_Database_11partC/signup.php
@@ -1,4 +1,5 @@
 <?php require_once "header.php";
+require_once "libs/pdo.php";
 ?>
 <form action="" method="post">
     <p>
@@ -14,7 +15,35 @@
         <input type="password" name="password" id="password"><br>
     </P>
     <button type="submit">sign up</button>
-
 </form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $username = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $checkQuery = $pdo->prepare("SELECT COUNT(*) FROM user WHERE email = :email");
+    $checkQuery->bindValue(':email', $email);
+    $checkQuery->execute();
+    $emailExist = $checkQuery->fetchColumn();
+
+    if ($emailExist > 0) {
+        echo "Email already exists.";
+    } else {
+        $hashpassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $query = $pdo->prepare("INSERT INTO user (nickname, email, password) VALUES (:name, :email, :password)");
+        $query->bindValue(':name', $username);
+        $query->bindValue(':email', $email);
+        $query->bindValue(':password', $hashpassword);
+        $query->execute();
+
+        echo "User registered successfully!";
+        header("Location: login");
+    }
+}
+?>
 <?php require_once "footer.php";
 ?>
